529193055a1a1b10c02d0154deb1b0e5793645ee
Ritesh Kumar Panda <ritesh.ku.panda.925@gmail.com>
2024-01-03 23:03:00 +0530

    Day 24

--- Length of Last Word.cpp
+++ Length of Last Word.cpp
@@ -1,5 +1,5 @@
-    int lengthOfLastWord(string s) {
-        int ans=0, n=s.size(), i;
+int lengthOfLastWord(string s) {
+    int ans=0, n=s.size(), i;
         for(i=n-1;i>=0;i--){
             if(s[i]!=' ') break;
         }
