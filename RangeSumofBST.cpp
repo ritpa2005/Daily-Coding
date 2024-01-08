ddd4163d4b49602d1a4e6d7982290c4bbafb8c66
Ritesh Kumar Panda <ritesh.ku.panda.925@gmail.com>
2024-01-08 21:47:52 +0530

    Day 29

---
+++ RangeSumofBST.cpp
@@ -0,0 +1,9 @@
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root==NULL){
+            return 0;
+        }
+        if(root->val >=low && root->val <=high){
+            return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+        }
+        return rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+    }
